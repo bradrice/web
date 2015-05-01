a7a59e6514e534dea6b2c433f55a368aa2f6a13b
Brad Rice <bradrice1@gmail.com>
2015-05-01 16:44:39 -0400

    settings_prod for online

--- web/web/urls.py
+++ web/web/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
      url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^ckeditor/', include('ckeditor.urls')),
-    url(r'^blog/', include('zinnia.urls', namespace='blog')),
+    url(r'^blog/', include('zinnia.urls', namespace='zinnia')),
     url(r'^comments/', include('django_comments.urls')),
+    url(r'^ckeditor/', include('ckeditor.urls')),
 ]
